3824adf64b2c83ccaa7ebfff2116db9228182bd8
Akbermet <akbermet.zh@gmail.com>
2021-03-22 00:28:15 +0100

    exercise 1.10 , without button component

--- src/App.js
+++ src/App.js
@@ -27,24 +27,54 @@ const AdditionalButtonStat = ({good, neutral, bad}) => {
 }
 
 const Statistics = ({good, neutral, bad}) => { // this component will contain all of the statistics (step 3) 
-  if(good > 0 || neutral > 0 || bad > 0)
-  {
-    return (
-      <div>
-        <ButtonStatistics buttonType="good" clickCount={good}/>
-        <ButtonStatistics buttonType="neutral" clickCount={neutral}/>
-        <ButtonStatistics buttonType="bad" clickCount={bad}/>
-        <AdditionalButtonStat good={good} neutral={neutral} bad={bad}/>
-      </div>
-    )
+  const positivePercent = (good, neutral, bad) => {
+    let total = good + neutral + bad
+    
+    if(total > 0)
+    {
+      return (good * 100)/total + "%"
+    }
+
+    return "0%"
   }
-  else
+
+  const average = (good, neutral, bad) => {
+    let total = 0, average = 0
+    total = good + neutral + bad
+    if(total > 0)
+    {
+      average = ((good*1) + (neutral*0) + (bad * -1))/ total
+    }
+
+    return average
+  }
+
+  if(good === 0 && neutral === 0 && bad === 0)
   {
     return (
       <p> no feedback given </p>
     )
   }
+  else
+  {
+    return (
+      <div>
+      <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+      <Statistic text="all" value={good+neutral+bad} />
+      <Statistic text="average" value={average(good, neutral, bad)} />
+      <Statistic text="positive" value={positivePercent(good, neutral, bad)} />
+      </div>
+    )
+  }
 
+}
+
+const Statistic = ({text, value}) => {
+  return (
+    <p> {text} {value}</p>
+  )
 }
 
 const App = () => {
@@ -65,6 +95,28 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const average = (good, neutral, bad) => {
+    let total = 0, average = 0
+    total = good + neutral + bad
+    if(total > 0)
+    {
+      average = ((good*1) + (neutral*0) + (bad * -1))/ total
+    }
+
+    return average
+  }
+
+  const positivePercent = (good, neutral, bad) => {
+    let total = good + neutral + bad
+    
+    if(total > 0)
+    {
+      return (good * 100)/total + "%"
+    }
+
+    return "0%"
+  }
+
   return (
     <div>
       <h1> Give feedback </h1>
@@ -74,6 +126,12 @@ const App = () => {
 
       <h1> Statistics </h1>
       <Statistics good={good} neutral={neutral} bad={bad}/>
+      {/* <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+      <Statistic text="all" value={good+neutral+bad} />
+      <Statistic text="average" value={average(good, neutral, bad)} />
+      <Statistic text="positive" value={positivePercent(good, neutral, bad)} /> */}
     </div>
   )
 }
